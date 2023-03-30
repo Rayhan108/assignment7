81430480474be37e91e2b14f3144368fa7d8e299
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-31 05:29:59 +0600

    display spent time from local storage

--- src/componets/BookmarkCard/BookmarkCard.jsx
+++ src/componets/BookmarkCard/BookmarkCard.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./BookmarkCard.css"
 
 const BookmarkCard = ({spentTime}) => {
+    const [time,setTime]=useState("spentTime")
+    useEffect(()=>{
+        const getSpentTimeFromLocalStorage=JSON.parse(localStorage.getItem("spent-time"));
+         if(getSpentTimeFromLocalStorage){
     
+          setTime(getSpentTimeFromLocalStorage);
+        }else{
+          setTime(0)
+        }
+    },[spentTime])
     
     return (
         <div className='bookmark-container'>
          <div className="bookmark">
-         <h4>Spent time on read : {spentTime}</h4>
+         <h4>Spent time on read : {time}</h4>
             <div className="bookmark-list">
                 <h4>Bookmarked Blogs :</h4>
             </div>
