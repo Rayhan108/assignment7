0753dd0b17eeae060de99d42a74b81586a82f7fd
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-31 00:11:59 +0600

    added header component

---
+++ src/componets/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.css"
+const Header = () => {
+    return (
+        <>
+      <nav className="header">
+      <h2>Knowledge</h2>
+      <div className='nav-link'>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/login">Login</a>
+        <img src="https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+      </div>
+    </nav>
+ 
+        </>
+    );
+};
+
+export default Header;
